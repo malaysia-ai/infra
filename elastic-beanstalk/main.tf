c0726302d4238612010c21ed81a705823de80ca8
Ariff Nazhan <ariff-zoala@bitbucket.org>
2023-09-17 13:53:31 +0800

    clean

--- elastic-beanstalk/main.tf
+++ elastic-beanstalk/main.tf
@@ -32,38 +32,35 @@ provider "aws" {
   region = "ap-southeast-1" # Change this to your desired AWS region
 }
 
-# Create S3 bucket for Python Flask app
-
+# https://www.youtube.com/watch?v=x2IN28DKK3o
 data "aws_s3_bucket" "eb_bucket" {
-  bucket = "elasticbeanstalk-ap-southeast-1-896280034829" # Name of S3 bucket to create for Flask app deployment needs to be unique 
+  bucket = "elasticbeanstalk-ap-southeast-1-896280034829"
 }
-
-# Define App files to be uploaded to S3
 data "aws_s3_object" "eb_bucket_obj" {
   bucket = data.aws_s3_bucket.eb_bucket.id
-  key    = "elasticbeanstalk-ap-southeast-1-896280034829/fastapi/app-230917_004110810067.zip" # S3 Bucket path to upload app files
+  key    = "elasticbeanstalk-ap-southeast-1-896280034829/fastapi/app-230917_004110810067.zip" 
 }
 
 # Define Elastic Beanstalk application
 resource "aws_elastic_beanstalk_application" "eb_app" {
-  name        = "fastapi-meso"   # Name of the Elastic Beanstalk application
-  description = "simple fastapi app" # Description of the Elastic Beanstalk application
+  name        = "fastapi-meso"
+  description = "simple fastapi app"
 }
 
-# Create Elastic Beanstalk environment for application with defining environment settings
+# Create Elastic Beanstalk environment
 resource "aws_elastic_beanstalk_application_version" "eb_app_ver" {
-  bucket      = data.aws_s3_bucket.eb_bucket.id                    # S3 bucket name
-  key         = data.aws_s3_object.eb_bucket_obj.id         # S3 key path 
-  application = aws_elastic_beanstalk_application.eb_app.name # Elastic Beanstalk application name
-  name        = "fastapiv1"                # Version label for Elastic Beanstalk application
+  bucket      = data.aws_s3_bucket.eb_bucket.id                    
+  key         = data.aws_s3_object.eb_bucket_obj.id         
+  application = aws_elastic_beanstalk_application.eb_app.name 
+  name        = "fastapiv1"                
 }
 
 resource "aws_elastic_beanstalk_environment" "tfenv" {
   name                = "fastapi-meso-env"
-  application         = aws_elastic_beanstalk_application.eb_app.name             # Elastic Beanstalk application name
-  solution_stack_name = "64bit Amazon Linux 2 v3.4.4 running Python 3.8"         # Define current version of the platform
-  description         = "environment for fastapi app"                               # Define environment description
-  version_label       = aws_elastic_beanstalk_application_version.eb_app_ver.name # Define version label
+  application         = aws_elastic_beanstalk_application.eb_app.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.4.4 running Python 3.8"    # Define current version of the platform
+  description         = "environment for fastapi app"
+  version_label       = aws_elastic_beanstalk_application_version.eb_app_ver.name
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration" # Define namespace
