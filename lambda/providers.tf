f1b6c1f876fab171c6a38bf867b7fa6b96061531
Ariff Nazhan <ariff-zoala@bitbucket.org>
2023-09-18 16:12:06 +0800

    initial commit

---
+++ lambda/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 0.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.19"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region  = "ap-southeast-1"
+}
---
+++ lambda/variables.tf
@@ -0,0 +1,9 @@
+locals {
+  lambda_build_path = "build"
+  lambda_zip_file_name = "aws-lambda.zip"
+  lambda_src_path = "./src"
+}
+
+variable "lambda_function_name" {
+  default = "lambda_function_name"
+}
