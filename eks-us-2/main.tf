5e536a4f4d52db8add3cdc9cc3c20d5d6c1944e7
Halim Shukor <58404177+cerbrus666@users.noreply.github.com>
2024-01-17 01:20:18 +0800

    add node6 again

--- eks-us-2/main.tf
+++ eks-us-2/main.tf
@@ -166,32 +166,11 @@ resource "aws_iam_role_policy_attachment" "nodegroup_attachment-ecr" {
 #
 # }
 
-
-
-resource "aws_eks_node_group" "node5" {
-  cluster_name    = aws_eks_cluster.cluster.name
-  node_group_name = "node5"
-  node_role_arn   = aws_iam_role.nodegroup.arn
-  subnet_ids      = [aws_default_subnet.subnet1.id, aws_default_subnet.subnet2.id, aws_default_subnet.subnet3.id]
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
-  }
-
-  ami_type = "AL2_x86_64_GPU"
-  capacity_type = "SPOT"
-  instance_types = ["trn1.2xlarge"]
-  disk_size = 100
-
-}
-
 resource "aws_eks_node_group" "node6" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "node6"
   node_role_arn   = aws_iam_role.nodegroup.arn
-  subnet_ids      = [aws_default_subnet.subnet1.id, aws_default_subnet.subnet2.id, aws_default_subnet.subnet3.id]
+  subnet_ids      = [aws_default_subnet.subnet2.id]
 
   scaling_config {
     desired_size = 1
