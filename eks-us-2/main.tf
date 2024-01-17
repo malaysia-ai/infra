c20b9c49677d59486b9f32a55c3450fd5ad7608a
Ariff Nazhan <ariff-zoala@bitbucket.org>
2024-01-17 15:41:10 +0800

    configure instance into all subnets

--- eks-us-2/main.tf
+++ eks-us-2/main.tf
@@ -127,77 +127,11 @@ resource "aws_iam_role_policy_attachment" "nodegroup_attachment-ecr" {
 #
 # }
 
-# resource "aws_eks_node_group" "node1" {
-#   cluster_name    = aws_eks_cluster.cluster.name
-#   node_group_name = "node1"
-#   node_role_arn   = aws_iam_role.nodegroup.arn
-#   subnet_ids      = [aws_default_subnet.subnet1.id, aws_default_subnet.subnet2.id, aws_default_subnet.subnet3.id]
-#
-#   scaling_config {
-#     desired_size = 1
-#     max_size     = 1
-#     min_size     = 1
-#   }
-#
-#   ami_type = "AL2_x86_64_GPU"
-#   capacity_type = "SPOT"
-#   instance_types = ["inf2.xlarge"]
-#   disk_size = 100
-#
-# }
-
-
-# resource "aws_eks_node_group" "node1" {
-#   cluster_name    = aws_eks_cluster.cluster.name
-#   node_group_name = "node1"
-#   node_role_arn   = aws_iam_role.nodegroup.arn
-#   subnet_ids      = [aws_default_subnet.subnet1.id, aws_default_subnet.subnet2.id, aws_default_subnet.subnet3.id]
-#
-#   scaling_config {
-#     desired_size = 1
-#     max_size     = 1
-#     min_size     = 1
-#   }
-#
-#   ami_type = "AL2_x86_64_GPU"
-#   capacity_type = "SPOT"
-#   instance_types = ["inf2.xlarge"]
-#   disk_size = 100
-#
-# }
-
-#  resource "aws_eks_node_group" "node7" {
-#   cluster_name    = aws_eks_cluster.cluster.name
-#   node_group_name = "node7"
-#   node_role_arn   = aws_iam_role.nodegroup.arn
-#   subnet_ids      = [aws_default_subnet.subnet2.id]
-# 
-#   labels = {
-#     arep = "owned" 
-#   }
-# 
-#   scaling_config {
-#     desired_size = 1
-#     max_size     = 1
-#     min_size     = 1
-#   }
-
-#   ami_type = "AL2_x86_64_GPU"
-#   capacity_type = "SPOT"
-#   instance_types = ["trn1.32xlarge"]
-#   disk_size = 100
-# 
-# }
-
-resource "aws_eks_node_group" "node1" {
+ resource "aws_eks_node_group" "node7" {
   cluster_name    = aws_eks_cluster.cluster.name
-  node_group_name = "node1"
+  node_group_name = "node7"
   node_role_arn   = aws_iam_role.nodegroup.arn
   subnet_ids      = [aws_default_subnet.subnet1.id, aws_default_subnet.subnet2.id, aws_default_subnet.subnet3.id]
-
-  labels = {
-    kamarul = "owned"
-  }
 
   scaling_config {
     desired_size = 1
@@ -207,10 +141,10 @@ resource "aws_eks_node_group" "node1" {
 
   ami_type = "AL2_x86_64_GPU"
   capacity_type = "SPOT"
-  instance_types = ["inf2.xlarge"]
+  instance_types = ["trn1.32xlarge"]
   disk_size = 100
-}
 
+}
 
 resource "aws_iam_openid_connect_provider" "this" {
   client_id_list = ["sts.amazonaws.com"]
