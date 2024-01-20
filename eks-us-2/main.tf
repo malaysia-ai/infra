331b268e7a7f68745a2ad2383ca341e73561b515
Adha Sahar <adhasahar97@gmail.com>
2024-01-20 23:49:43 +0800

    Node arep

--- eks-us-2/main.tf
+++ eks-us-2/main.tf
@@ -160,46 +160,46 @@ resource "aws_iam_role_policy_attachment" "nodegroup_attachment-ecr" {
 #
 #}
 
-resource "aws_eks_node_group" "node-a100" {
-  cluster_name    = aws_eks_cluster.cluster.name
-  node_group_name = "node-a100"
-  node_role_arn   = aws_iam_role.nodegroup.arn
-  subnet_ids      = [aws_default_subnet.subnet1.id, aws_default_subnet.subnet2.id, aws_default_subnet.subnet3.id]
+# resource "aws_eks_node_group" "node-a100" {
+#   cluster_name    = aws_eks_cluster.cluster.name
+#   node_group_name = "node-a100"
+#   node_role_arn   = aws_iam_role.nodegroup.arn
+#   subnet_ids      = [aws_default_subnet.subnet1.id, aws_default_subnet.subnet2.id, aws_default_subnet.subnet3.id]
 
-  scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
-  }
+#   scaling_config {
+#     desired_size = 1
+#     max_size     = 1
+#     min_size     = 1
+#   }
 
-  ami_type = "AL2_x86_64_GPU"
-  capacity_type = "ON_DEMAND"
-  instance_types = ["p4d.24xlarge"]
-  disk_size = 100
+#   ami_type = "AL2_x86_64_GPU"
+#   capacity_type = "ON_DEMAND"
+#   instance_types = ["p4d.24xlarge"]
+#   disk_size = 100
 
-}
+# }
 
-resource "aws_eks_node_group" "node-trainium-2" {
-  cluster_name    = aws_eks_cluster.cluster.name
-  node_group_name = "node-trainium-2"
-  node_role_arn   = aws_iam_role.nodegroup.arn
-  subnet_ids      = [aws_default_subnet.subnet2.id]
+# resource "aws_eks_node_group" "node-trainium-2" {
+#   cluster_name    = aws_eks_cluster.cluster.name
+#   node_group_name = "node-trainium-2"
+#   node_role_arn   = aws_iam_role.nodegroup.arn
+#   subnet_ids      = [aws_default_subnet.subnet2.id]
 
-  labels = {
-    ariff = "owned"
-  }
-  scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
-  }
+#   labels = {
+#     ariff = "owned"
+#   }
+#   scaling_config {
+#     desired_size = 1
+#     max_size     = 1
+#     min_size     = 1
+#   }
 
-  ami_type = "AL2_x86_64_GPU"
-  capacity_type = "SPOT"
-  instance_types = ["trn1.2xlarge"]
-  disk_size = 100
+#   ami_type = "AL2_x86_64_GPU"
+#   capacity_type = "SPOT"
+#   instance_types = ["trn1.2xlarge"]
+#   disk_size = 100
 
-}
+# }
 
 resource "aws_eks_node_group" "devops-nodegroup" {
   cluster_name    = aws_eks_cluster.cluster.name
