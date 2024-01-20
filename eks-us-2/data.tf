5b529aa11aca9c6b23fbc875782ff3d3fb21d234
Adha Sahar <adhasahar97@gmail.com>
2024-01-21 00:10:36 +0800

    Enable oidc on deployment-3 cluster

--- eks-us-2/data.tf
+++ eks-us-2/data.tf
@@ -4,3 +4,9 @@ data "tls_certificate" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.cluster.name
 }
+data "tls_certificate" "deployment-3" {
+  url = aws_eks_cluster.deployment-3.identity[0].oidc[0].issuer
+}
+data "aws_eks_cluster_auth" "deployment-3" {
+  name = aws_eks_cluster.cluster.name
+}
