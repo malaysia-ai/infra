f1faf0cbc8492f56ed690f39def2bb65ffcff13c
Adha Sahar <adhasahar97@gmail.com>
2024-01-22 19:08:06 +0800

    Install nginx ingress controller

--- eks-us-2/helm-nginx.tf
+++ eks-us-2/helm-nginx.tf
@@ -1,15 +1,15 @@
-resource "kubernetes_namespace" "nginx-ingress" {
+resource "kubernetes_namespace" "ingress-nginx" {
   metadata {
-    name = "nginx-ingress"
+    name = "ingress-nginx"
   }
 }
 
-resource "helm_release" "nginx-ingress" {
-    name       = "nginx-ingress"
-    chart      = "nginx-ingress"
+resource "helm_release" "ingress-nginx" {
+    name       = "ingress-nginx"
+    chart      = "ingress-nginx"
     repository = "https://kubernetes.github.io/ingress-nginx"
     version    = "4.9.0"
-    namespace  = kubernetes_namespace.nginx-ingress.id
+    namespace  = kubernetes_namespace.ingress-nginx.id
 
     values = [templatefile("nginx-helm/values.yaml", {
        
