24ad7fdfedafeb40933692c23046e36253a8767d
Ariff Nazhan <58643675+oscarnz@users.noreply.github.com>
2024-01-25 03:13:36 +0800

    deploy trainium-2

--- eks-us-2/main.tf
+++ eks-us-2/main.tf
@@ -229,27 +229,27 @@ resource "aws_eks_addon" "eks-pod-identity-agent-addons" {
 
 # }
 
-# resource "aws_eks_node_group" "node-trainium-2" {
-#   cluster_name    = aws_eks_cluster.cluster.name
-#   node_group_name = "node-trainium-2"
-#   node_role_arn   = aws_iam_role.nodegroup.arn
-#   subnet_ids      = [aws_default_subnet.subnet2.id]
+resource "aws_eks_node_group" "node-trainium-2" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "node-trainium-2"
+  node_role_arn   = aws_iam_role.nodegroup.arn
+  subnet_ids      = [aws_default_subnet.subnet2.id]
 
-#   labels = {
-#     ariff = "owned"
-#   }
-#   scaling_config {
-#     desired_size = 1
-#     max_size     = 1
-#     min_size     = 1
-#   }
+  labels = {
+    ariff = "owned"
+  }
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
 
-#   ami_type = "AL2_x86_64_GPU"
-#   capacity_type = "SPOT"
-#   instance_types = ["trn1.2xlarge"]
-#   disk_size = 100
+  ami_type = "AL2_x86_64_GPU"
+  capacity_type = "SPOT"
+  instance_types = ["trn1.2xlarge"]
+  disk_size = 100
 
-# }
+}
 
 # resource "aws_eks_node_group" "node-trainium-3" {
 #   cluster_name    = aws_eks_cluster.deployment-4.name
