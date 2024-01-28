ffab54cb6e95cf92885399299657bb25423de9af
Halim Shukor <58404177+cerbrus666@users.noreply.github.com>
2024-01-28 21:09:06 +0800

    enable node7 deployment2

--- eks-us-2/main.tf
+++ eks-us-2/main.tf
@@ -291,24 +291,25 @@ resource "aws_eks_node_group" "devops-nodegroup" {
   }
 }
 
-#  resource "aws_eks_node_group" "node7" {
-#   cluster_name    = aws_eks_cluster.cluster.name
-#   node_group_name = "node7"
-#   node_role_arn   = aws_iam_role.nodegroup.arn
-#   subnet_ids      = [aws_default_subnet.subnet2.id]
+resource "aws_eks_node_group" "node7" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "node7"
+  node_role_arn   = aws_iam_role.nodegroup.arn
+  subnet_ids      = [aws_default_subnet.subnet2.id]
 
-#   scaling_config {
-#     desired_size = 1
-#     max_size     = 1
-#     min_size     = 1
-#   }
-
-#   ami_type = "AL2_x86_64_GPU"
-#   capacity_type = "SPOT"
-#   instance_types = ["trn1.32xlarge"]
-#   disk_size = 100
-
-# }
+  labels = {
+    mindscape = "owned"
+  }
+  scaling_config {
+     desired_size = 1
+     max_size     = 1
+     min_size     = 1
+   }
+  ami_type = "AL2_x86_64_GPU"
+  capacity_type = "SPOT"
+  instance_types = ["inf1.xlarge"]
+  disk_size = 100
+}
 
 resource "aws_iam_openid_connect_provider" "this" {
   client_id_list = ["sts.amazonaws.com"]
