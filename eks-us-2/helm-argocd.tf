83889f11e9989d738721d69974e4fb3b71535243
Adha Sahar <adhasahar97@gmail.com>
2024-02-06 12:02:48 +0800

    Upgrade argocd helm version

--- eks-us-2/helm-argocd.tf
+++ eks-us-2/helm-argocd.tf
@@ -7,7 +7,7 @@ resource "helm_release" "argocd" {
     name       = "argo-cd"
     chart      = "argo-cd"
     repository = "https://argoproj.github.io/argo-helm"
-    version    = "5.51.6"
+    version    = "5.53.14"
     namespace  = kubernetes_namespace.argocd.id
 
     values = [templatefile("argocd-helm/values.yaml", {
