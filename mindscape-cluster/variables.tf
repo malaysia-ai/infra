8211ee2d5e0e2098e85f2c6afcd89b43b1ec1ee5
Halim Shukor <58404177+cerbrus666@users.noreply.github.com>
2024-02-09 23:32:48 +0800

    incomment var

--- mindscape-cluster/variables.tf
+++ mindscape-cluster/variables.tf
@@ -1,13 +1,12 @@
-#variable "access_key" {
-#  type = string
-#}
-#
-#variable "secret_key" {
-#  type = string
-#}
-#
-#variable "region" {
-#  type    = string
-#  default = "us-west-2"
-#}
-#
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "us-west-2"
+}
