d9f361361c76677465ae07d8c7a103fa5d334f75
Halim Shukor <58404177+cerbrus666@users.noreply.github.com>
2024-02-09 23:31:25 +0800

    clean

--- mindscape-cluster/main.tf
+++ mindscape-cluster/main.tf
@@ -1,30 +1,30 @@
-#terraform {
-#
-#  cloud {
-#    organization = "malaysia-ai"
-#
-#    workspaces {
-#      name = "mindscape-cluster"
-#    }
-#  }
-#
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "~> 5.31.0"
-#    }
-#  }
-#
-#  required_version = ">= 1.1.0"
-#}
-#
-#provider "aws" {
-#  access_key = var.access_key
-#  secret_key = var.secret_key
-#  region     = var.region
-#}
-#
-#
+terraform {
+
+  cloud {
+    organization = "malaysia-ai"
+
+    workspaces {
+      name = "mindscape-cluster"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+}
+
+
 #data "aws_iam_policy_document" "assume_role" {
 #  statement {
 #    effect = "Allow"
