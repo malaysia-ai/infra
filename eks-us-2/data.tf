f98260b184178506a5f1f1fd4af385ea671b3555
Adha Sahar <adhasahar97@gmail.com>
2024-02-14 18:11:41 +0800

    Hardcode subnet id

--- eks-us-2/data.tf
+++ eks-us-2/data.tf
@@ -16,3 +16,13 @@ data "aws_subnets" "us-west-2-subnets" {
     values = ["vpc-038c715c3ba51de5b"]
   }
 }
+
+data "aws_subnet" "default_az_public_a" {
+  id = "subnet-08c83b443d6185bd5"
+}
+data "aws_subnet" "default_az_public_b" {
+  id = "subnet-0bf64f00fce048ca1"
+}
+data "aws_subnet" "default_az_public_c" {
+  id = "subnet-0440bbc5ba8484cef"
+}
--- eks-us-2/efs.tf
+++ eks-us-2/efs.tf
@@ -9,13 +9,13 @@ resource "aws_efs_file_system" "jupyter" {
 
 resource "aws_efs_mount_target" "jupyter-west-2a" {
   file_system_id = aws_efs_file_system.jupyter.id
-  subnet_id      = aws_default_subnet.subnet1.id
+  subnet_id      = data.aws_subnet.default_az_public_a.id
 }
 resource "aws_efs_mount_target" "jupyter-west-2b" {
   file_system_id = aws_efs_file_system.jupyter.id
-  subnet_id      = aws_default_subnet.subnet2.id
+  subnet_id      = data.aws_subnet.default_az_public_b.id
 }
 resource "aws_efs_mount_target" "jupyter-west-2c" {
   file_system_id = aws_efs_file_system.jupyter.id
-  subnet_id      = aws_default_subnet.subnet3.id
+  subnet_id      = data.aws_subnet.default_az_public_c.id
 }
