c1ab1afab7a740bb04dfd81c9234143cba88f37d
Adha Sahar <adhasahar97@gmail.com>
2024-02-14 10:02:45 +0800

    Delete trainium

--- eks-us-2/main.tf
+++ eks-us-2/main.tf
@@ -313,27 +313,27 @@ resource "aws_eks_addon" "eks-pod-identity-agent-addons" {
 
 # }
 
-resource "aws_eks_node_group" "trainium" {
-   cluster_name    = aws_eks_cluster.deployment-3.name
-   node_group_name = "trainium"
-   node_role_arn   = aws_iam_role.nodegroup.arn
-   subnet_ids      = [aws_default_subnet.subnet2.id]
+# resource "aws_eks_node_group" "trainium" {
+#    cluster_name    = aws_eks_cluster.deployment-3.name
+#    node_group_name = "trainium"
+#    node_role_arn   = aws_iam_role.nodegroup.arn
+#    subnet_ids      = [aws_default_subnet.subnet2.id]
 
-  labels = {
-      kamarul = "owned"
-   }
+#   labels = {
+#       kamarul = "owned"
+#    }
 
-   scaling_config {
-      desired_size = 1
-      max_size     = 1
-      min_size     = 1
-   }
+#    scaling_config {
+#       desired_size = 1
+#       max_size     = 1
+#       min_size     = 1
+#    }
 
-   ami_type = "AL2_x86_64_GPU"
-   capacity_type = "SPOT"
-   instance_types = ["trn1.2xlarge"]
-   disk_size = 100
-  }
+#    ami_type = "AL2_x86_64_GPU"
+#    capacity_type = "SPOT"
+#    instance_types = ["trn1.2xlarge"]
+#    disk_size = 100
+# }
 
 resource "aws_eks_node_group" "devops-nodegroup" {
   cluster_name    = aws_eks_cluster.deployment-3.name
