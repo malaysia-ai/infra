f890be193c2bafa40cbb0a0c5e186100f505e179
huseinzol05 <husein.zol05@gmail.com>
2024-02-25 22:34:54 +0800

    fix efs

--- eks-us-2/main.tf
+++ eks-us-2/main.tf
@@ -245,7 +245,7 @@ data "aws_iam_policy_document" "efs_cni_controller_deployment-3" {
     condition {
       test     = "StringEquals"
       variable = "${replace(aws_iam_openid_connect_provider.deployment-3.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+      values   = ["system:serviceaccount:kube-system:efs-csi-*"]
     }
   }
 }
