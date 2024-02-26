c53fc0c19bcf5a79ccc8e795fb94955dfe9eb3ae
huseinzol05 <husein.zol05@gmail.com>
2024-02-26 21:05:53 +0800

    comment efs

--- eks-us-2/efs.tf
+++ eks-us-2/efs.tf
@@ -1,11 +1,11 @@
-resource "aws_efs_file_system" "jupyter" {
-  creation_token = "jupyter-usage"
+# resource "aws_efs_file_system" "jupyter" {
+#   creation_token = "jupyter-usage"
 
-  tags = {
-    Name = "jupyter"
-  }
-  throughput_mode = "elastic"
-}
+#   tags = {
+#     Name = "jupyter"
+#   }
+#   throughput_mode = "elastic"
+# }
 
 # resource "aws_efs_mount_target" "jupyter-west-2a" {
 #   file_system_id = aws_efs_file_system.jupyter.id
