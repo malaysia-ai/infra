9aacbf9c3046de0fbe205ce64071854d555dc672
huseinzol05 <husein.zol05@gmail.com>
2024-02-26 21:08:12 +0800

    remove outputs

--- eks-us-2/outputs.tf
+++ eks-us-2/outputs.tf
@@ -1,7 +1,7 @@
-output "default_subned_ids" {
-  value = data.aws_subnets.us-west-2-subnets.ids
-}
+# output "default_subned_ids" {
+#   value = data.aws_subnets.us-west-2-subnets.ids
+# }
 
-output "jupyter-efs-id" {
-  value = aws_efs_file_system.jupyter.id
-}
+# output "jupyter-efs-id" {
+#   value = aws_efs_file_system.jupyter.id
+# }
