9bf06716212ef966b6a840a92636b5332d644e4a
Adha Sahar <adhasahar97@gmail.com>
2024-02-28 13:07:48 +0800

    Mount efs

--- eks-us-2/efs.tf
+++ eks-us-2/efs.tf
@@ -1,21 +1,21 @@
-# resource "aws_efs_file_system" "jupyter" {
-#   creation_token = "jupyter-usage"
+resource "aws_efs_file_system" "jupyter" {
+  creation_token = "jupyter-usage"
 
-#   tags = {
-#     Name = "jupyter"
-#   }
-#   throughput_mode = "elastic"
-# }
+  tags = {
+    Name = "jupyter"
+  }
+  throughput_mode = "elastic"
+}
 
-# resource "aws_efs_mount_target" "jupyter-west-2a" {
-#   file_system_id = aws_efs_file_system.jupyter.id
-#   subnet_id      = data.aws_subnet.default_az_public_a.id
-# }
-# resource "aws_efs_mount_target" "jupyter-west-2b" {
-#   file_system_id = aws_efs_file_system.jupyter.id
-#   subnet_id      = data.aws_subnet.default_az_public_b.id
-# }
-# resource "aws_efs_mount_target" "jupyter-west-2c" {
-#   file_system_id = aws_efs_file_system.jupyter.id
-#   subnet_id      = data.aws_subnet.default_az_public_c.id
-# }
+resource "aws_efs_mount_target" "jupyter-west-2a" {
+  file_system_id = aws_efs_file_system.jupyter.id
+  subnet_id      = data.aws_subnet.default_az_public_a.id
+}
+resource "aws_efs_mount_target" "jupyter-west-2b" {
+  file_system_id = aws_efs_file_system.jupyter.id
+  subnet_id      = data.aws_subnet.default_az_public_b.id
+}
+resource "aws_efs_mount_target" "jupyter-west-2c" {
+  file_system_id = aws_efs_file_system.jupyter.id
+  subnet_id      = data.aws_subnet.default_az_public_c.id
+}
