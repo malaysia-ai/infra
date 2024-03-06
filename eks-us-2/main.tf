40bb0c5ba4bce86528082b8787544c2bde4f496a
Halim Shukor <58404177+mindscapexyz@users.noreply.github.com>
2024-03-06 22:09:08 +0800

    run trainium

--- eks-us-2/main.tf
+++ eks-us-2/main.tf
@@ -373,27 +373,27 @@ resource "aws_eks_addon" "eks-pod-identity-agent-addons" {
 #   disk_size      = 100
 # }
 
-# resource "aws_eks_node_group" "inferentia" {
-#   cluster_name    = aws_eks_cluster.deployment-3.name
-#   node_group_name = "inferentia"
-#   node_role_arn   = aws_iam_role.nodegroup.arn
-#   subnet_ids      = [aws_default_subnet.subnet2.id]
+ resource "aws_eks_node_group" "inferentia" {
+   cluster_name    = aws_eks_cluster.deployment-3.name
+   node_group_name = "inferentia"
+   node_role_arn   = aws_iam_role.nodegroup.arn
+   subnet_ids      = [aws_default_subnet.subnet2.id]
 
-#   labels = {
-#     inferentia = "owned"
-#   }
+   labels = {
+     inferentia = "owned"
+   }
 
-#   scaling_config {
-#     desired_size = 1
-#     max_size     = 1
-#     min_size     = 1
-#   }
+   scaling_config {
+     desired_size = 1
+     max_size     = 1
+     min_size     = 1
+   }
 
-#   ami_type       = "AL2_x86_64_GPU"
-#   capacity_type  = "SPOT"
-#   instance_types = ["inf2.xlarge"]
-#   disk_size      = 100
-# }
+   ami_type       = "AL2_x86_64_GPU"
+   capacity_type  = "SPOT"
+   instance_types = ["inf2.xlarge"]
+   disk_size      = 100
+ }
 
 resource "aws_eks_node_group" "devops-nodegroup" {
   cluster_name    = aws_eks_cluster.deployment-3.name
