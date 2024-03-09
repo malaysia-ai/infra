f4012bccd36c2ba18e8ba21cbbe0497bac380fd1
Ariff Nazhan <ariff-zoala@bitbucket.org>
2024-03-09 15:46:53 +0800

    add write item endpoint

--- fastapi-hello-world/main.py
+++ fastapi-hello-world/main.py
@@ -13,3 +13,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/write/items/{item_id}")
+def write_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
