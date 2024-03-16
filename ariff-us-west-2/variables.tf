c5b89585ed929af18eab6467afd598a2c938efbf
Ariff Nazhan <ariff-zoala@bitbucket.org>
2024-03-16 23:25:47 +0800

    change region and subnet id

--- ariff-us-west-2/variables.tf
+++ ariff-us-west-2/variables.tf
@@ -11,23 +11,23 @@ variable "secret_key" {
 variable "region" {
   description = "AWS Region"
   type        = string
-  default     = "us-west-2"
+  default     = "ap-southeast-1"
 }
 
 variable "subnet_id_1" {
   description = "subnet id 1"
   type        = string
-  default     = "subnet-0a36743bf1aa1ab50"
+  default     = "subnet-03e2e6d05407d4e38"
 }
 
 variable "subnet_id_2" {
   description = "subnet id 2"
   type        = string
-  default     = "subnet-0440bbc5ba8484cef"
+  default     = "subnet-0ad78399087fe3ef3"
 }
 
 variable "subnet_id_3" {
   description = "subnet id 3"
   type        = string
-  default     = "subnet-08c83b443d6185bd5"
+  default     = "subnet-050920cd49df6e67f"
 }
